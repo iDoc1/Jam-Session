19c0e20f1ece074f465240c907298511ecbda24f
nbowd <bowdenn@oregonstate.edu>
2022-10-15 15:51:52 -0700

    refactored SignInForm to a functional component from a class-based componenent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import SignUp  from './components/SignUpForm'
-import { SignIn } from './components/SignInForm'
+import SignIn from './components/SignInForm'
 
 function App() {
   return (
-    <SignUp/>
-    // <SignIn/>
+    // <SignUp/>
+    <SignIn/>
   )
 }
 
--- frontend/src/components/SignInForm.tsx
+++ frontend/src/components/SignInForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css"
 
 const emailRegex = RegExp(/^\s?[A-Z0–9]+[A-Z0–9._+-]{0,}@[A-Z0–9._+-]+\.[A-Z0–9]{2,4}\s?$/i);
@@ -14,79 +14,61 @@ interface SignInState {
     errors : string
 }
 
-export class SignIn extends React.Component<SignInProps, SignInState>{
-    constructor(props: SignInProps) {
-        super(props)
-        const initialState = {
-            email : '',
-            password : '',
-            errors : ''
-        }
-        this.state = initialState;
-        this.handleChange = this.handleChange.bind(this)
-    }
-    handleChange = (event: any) => {
+function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('')
+
+    const handleChange = (event: any) => {
         event.preventDefault()
         const { name, value } = event.target;
-        let errors = this.state.errors;
-        // switch( name ) {
-        //     case 'email':
-        //         errors.email = emailRegex.test(value)? '': 'Email is not valid!';
-        //         break
-        //     case 'password':
-        //         errors.password = value.length < 8 ? 'Password must be eight characters long!': '';
-        //         break
-        //     default:
-        //         break
-        // }
-        this.setState(Object.assign(this.state, {errors, [name]: value}))
-        console.log(this.state)
-    }
-    handleSubmit = (event:any) => {
-        event.preventDefault();
-        let errors = this.state.errors;
-
-        if (!this.state.email || !this.state.password) {
-           
-            this.setState({
-                errors: 'Missing Username and/or Password'
-            })
-
-            // this.state.errors.credentials = 'Missing Username and/or Password'
+        switch( name ) {
+            case 'email':
+                setEmail(value)
+                break
+            case 'password':
+                setPassword(value)
+                break
+            default:
+                break
         }
-        console.log(this.state);
-        
-
+    }
+    const handleSubmit = (event:any) => {
+        event.preventDefault();
+        if (!email || !password) {           
+            setError('Missing Username and/or Password')
+            setTimeout(() =>{
+                setError('');
+            }, 3000)
+        }
     }
 
-    render() {
-        const { errors } = this.state
-        return (
-          <div className='wrapper'>
-            <div className='form-wrapper'>
-               <h2>Sign In</h2>
-               <form onSubmit={this.handleSubmit} noValidate >
-                  <div className='email'>
-                     <label htmlFor="email">Email</label>
-                     <input type='email' name='email' onChange={this.handleChange}/>
-                  </div>
-                  <div className='password'>
-                     <label htmlFor="password">Password</label>
-                     <input type='password' name='password' onChange={this.handleChange}/>
-                  </div>              
-                  <div className='submit'>
-                     <button>Register Me</button>
-                     {errors.length > 0 && <span style={{color: "red"}}>{errors}</span>}
-                  </div>
-                  <div className='needAccount'>
-                    <p>Need an account? <br/></p>
-                    <span>
-                        <a href="#">Sign Up</a>
-                    </span>
-                  </div>              
-             </form>
-         </div>
-      </div>
-     );
-    }
-}
+    return (
+        <div className='wrapper'>
+        <div className='form-wrapper'>
+            <h2>Sign In</h2>
+            <form onSubmit={handleSubmit} noValidate >
+                <div className='email'>
+                    <label htmlFor="email">Email</label>
+                    <input type='email' name='email' onChange={handleChange}/>
+                </div>
+                <div className='password'>
+                    <label htmlFor="password">Password</label>
+                    <input type='password' name='password' onChange={handleChange}/>
+                </div>              
+                <div className='submit'>
+                    <button>Register Me</button>
+                    {error.length > 0 && <span style={{color: "red"}}>{error}</span>}
+                </div>
+                <div className='needAccount'>
+                <p>Need an account? <br/></p>
+                <span>
+                    <a href="#">Sign Up</a>
+                </span>
+                </div>              
+            </form>
+        </div>
+    </div>
+    );
+};
+export default SignIn
