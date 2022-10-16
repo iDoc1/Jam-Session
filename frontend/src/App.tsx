ef7726f78217cfced98736563bcb2f8beca8322b
nbowd <bowdenn@oregonstate.edu>
2022-10-16 16:43:51 -0700

    restored app to original state after experimenting with testing options

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import SignUp  from './components/SignUpForm'
-import SignIn from './components/SignInForm'
+import SignUp from './components/SignUpForm';
+import SignIn from './components/SignInForm';
 
 function App() {
   return (
-    // <SignUp/>
-    <SignIn/>
-  )
+    <SignUp />
+    // <SignIn />
+  );
 }
 
 export default App;
