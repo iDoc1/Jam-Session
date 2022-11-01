9b66b93917bc386b4df7d9b35652fd2b212f7f1d
iDoc1 <ianardocherty@gmail.com>
2022-10-31 20:14:04 -0700

    heroku deployment, removed locahost from fetch again in build only

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -215,7 +215,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication'  # For DRF browsable API
+        # 'rest_framework.authentication.SessionAuthentication'  # For DRF browsable API, remove for Prod
     ],
 }
 
