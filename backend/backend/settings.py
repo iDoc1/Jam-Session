01220bf30aabc3bc368633fa4b83e9dd226a87da
nbowd <bowdenn@oregonstate.edu>
2022-11-02 21:27:07 -0700

    commented out sessionAuthentication in backend/settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -189,7 +189,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication'  # For DRF browsable API
+        # 'rest_framework.authentication.SessionAuthentication'  # For DRF browsable API
     ],
 }
 
