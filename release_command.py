be7eb1d265c03d9197a1c0098ba39caf3f2a01a4
iDoc1 <ianardocherty@gmail.com>
2022-11-08 20:21:24 -0800

    heroku deployment

--- release_command.py
+++ release_command.py
@@ -2,20 +2,15 @@
 This file defines the commands to be run prior to the app being released on Heroku
 """
 import os
-import shutil
 
 
 def migrate_database():
     """
-    Applies Django database migrations and runs npm build command
+    Applies Django database migrations
     """
 
-    # Move build directory to backend directory
-    os.chdir('./frontend')
-    shutil.move('./build', '../backend')
-
     # Apply database migrations
-    os.chdir('../backend')
+    os.chdir('./backend')
     os.system('python manage.py migrate')
 
 
