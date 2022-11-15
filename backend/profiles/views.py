f6e928ff92509b0373f70230d686dca9e1904008
iDoc1 <ianardocherty@gmail.com>
2022-11-14 18:48:46 -0800

    refactored to allow users to see non-sensitive fields of other users' profiles

--- backend/profiles/views.py
+++ backend/profiles/views.py
@@ -3,7 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 from .models import (
     ExperienceLevel,
     UserInstrument,
-    UserProfile, Gender,
+    UserProfile,
+    Gender,
     CommitmentLevel,
     SocialMediaLink
 )
@@ -13,7 +14,8 @@ from .serializers import (
     UserProfileSerializer,
     GenderSerializer,
     CommitmentLevelSerializer,
-    SocialMediaLinkSerializer
+    SocialMediaLinkSerializer,
+    PublicUserProfileSerializer
 )
 
 
@@ -70,7 +72,7 @@ class UserProfileViewSet(ModelViewSet):
     """
     serializer_class = UserProfileSerializer
 
-    # PUT not allowed since only some fields of UserProfile alloed to be updated
+    # PUT not allowed since only some fields of UserProfile allowed to be updated
     http_method_names = ['get', 'patch']
 
     def get_queryset(self):
@@ -80,6 +82,28 @@ class UserProfileViewSet(ModelViewSet):
         user = self.request.user
         return UserProfile.objects.filter(user=user)
 
+    def retrieve(self, request, *args, **kwargs):
+        """
+        If the requesting user and the requested profile match, then returns the entire
+        UserProfile. If the user and requested profile do not match, returns only the
+        fields that are publicly available.
+        """
+        user = request.user
+        requested_profile_id = int(kwargs.get('pk'))
+        profile = UserProfile.objects.get(user=user)
+
+        if profile.id == requested_profile_id:
+            serializer = UserProfileSerializer(profile)
+        else:
+            try:
+                profile = UserProfile.objects.get(id=requested_profile_id)
+            except UserProfile.DoesNotExist:
+                return Response({'detail': 'requested profile does not exist'}, status=404)
+
+            serializer = PublicUserProfileSerializer(profile)
+
+        return Response(serializer.data)
+
     def list(self, request, *args, **kwargs):
         """
         Returns a single UserProfile when a list is requested
@@ -88,3 +112,16 @@ class UserProfileViewSet(ModelViewSet):
         profile = UserProfile.objects.get(user=user)
         serializer = UserProfileSerializer(profile)
         return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Prevent unauthorized user from updating another user's profile
+        """
+        user = request.user
+        requested_profile_id = int(kwargs.get('pk'))
+        profile = UserProfile.objects.get(user=user)
+
+        if profile.id != requested_profile_id:
+            return Response({'detail': 'requestor is not authorized to edit profile that is not their own'}, status=401)
+
+        return super().update(request, *args, **kwargs)
